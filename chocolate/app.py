827387117cb7ec788d73be82c52bb9ea52eb1d10
Abrahamemas <87199699+abrahamemas@users.noreply.github.com>
2022-12-05 19:49:38 +0100

    'Third commit'

---
+++ chocolate/app.py
@@ -0,0 +1,54 @@
+from numpy.core.numeric import indices
+import streamlit as st
+import tensorflow as tf
+import streamlit as st
+
+
+st.cache()
+def load_model():
+  model=tf.keras.models.load_model('model2.h5')
+  return model
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+
+st.write("""
+         # Chocolate Classification
+         """
+         )
+
+file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+st.set_option('deprecation.showfileUploaderEncoding', False)
+def import_and_predict(image_data, model):
+    
+        size = (224,224)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+        
+        img_reshape = img[np.newaxis,...]
+    
+        prediction = model.predict(img_reshape)
+        return prediction
+    
+     
+    
+if file == None:
+    st.text("Please upload an image file")
+elif file == 1 :
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    score = tf.nn.softmax(predictions[0])
+    st.write(predictions)
+    st.write(score)
+    
+    class_names = model.class_indices
+    
+    st.write(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
